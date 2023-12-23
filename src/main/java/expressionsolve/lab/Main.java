f76b256efb003a7bcca665c770662c2a0b147bf0
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-23 05:30:39 +0300

    Update Main.java

--- src/main/java/expressionsolve/lab/Main.java
+++ src/main/java/expressionsolve/lab/Main.java
@@ -1,19 +1,22 @@
 package expressionsolve.lab;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
 public class Main {
+    private final static String firstPath = "src/main/resources/first.properties";
+    private final static String secondPath = "src/main/resources/second.properties";
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        SomeBean someBean = null;
+        try {
+            someBean = (new Injector().inject(new SomeBean(), firstPath));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        someBean.foo();
+
+        try {
+            someBean = (new Injector().inject(new SomeBean(), secondPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        someBean.foo();
     }
 }
