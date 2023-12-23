ba8589c96758f48a7f29ce3946ee2d857e87b9a8
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-23 05:32:27 +0300

    SomeBean.java

--- src/main/java/expressionsolve/lab/SomeBean.java
+++ src/main/java/expressionsolve/lab/SomeBean.java
@@ -1,2 +1,30 @@
-package expressionsolve.lab;public class SomeBean {
+package expressionsolve.lab;
+/**
+ * @author Алексей Стариков
+ */
+public class SomeBean {
+    /**
+     * Конструктор для класса SomeBean
+     */
+    public SomeBean() {}
+
+    /**
+     * Автоматически вводимое поле, представляющее SomeInterface
+     */
+    @AutoInjectable
+    private SomeInterface field1;
+
+    /**
+     *  Автоматически вводимое поле, представляющее OtherInterface
+     */
+    @AutoInjectable
+    private OtherInterface field2;
+
+    /**
+     * Выполняет действие, связанное с введенными полями
+     */
+    public void foo() {
+        field1.doSomething();
+        field2.doSomething();
+    }
 }
