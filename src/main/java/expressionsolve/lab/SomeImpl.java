3941e7d63d0e9e3ffefad0ebb3d28aceb6a311c0
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-23 05:32:56 +0300

    SomeImpl.java

--- src/main/java/expressionsolve/lab/SomeImpl.java
+++ src/main/java/expressionsolve/lab/SomeImpl.java
@@ -1,2 +1,17 @@
-package expressionsolve.lab;public class SomeImpl {
+package expressionsolve.lab;
+/**
+ * @author Алексей Стариков
+ */
+public class SomeImpl implements SomeInterface {
+    /**
+     * Конструктор для класса SomeImpl
+     */
+    public SomeImpl() {}
+
+    /**
+     * Выполняет действие, связанное с этой реализацией, выводя в консоль "A"
+     */
+    public void doSomething() {
+        System.out.println("A");
+    }
 }
