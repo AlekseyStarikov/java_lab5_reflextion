950f32067023fdcf54143768fe8938c911cc341a
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-23 05:32:07 +0300

    SoDoer.java

--- src/main/java/expressionsolve/lab/SoDoer.java
+++ src/main/java/expressionsolve/lab/SoDoer.java
@@ -1,2 +1,17 @@
-package expressionsolve.lab;public class SoDoer {
+package expressionsolve.lab;
+/**
+ * @author Алексей Стариков
+ */
+public class SoDoer implements OtherInterface {
+    /**
+     * Конструктор для класса SoDoer
+     */
+    public SoDoer() {}
+    /**
+     *Выполняет действие, связанное с этой реализацией, выводя в консоль "C"
+     */
+    public void doSomething() {
+        System.out.println("C");
+    }
 }
+
