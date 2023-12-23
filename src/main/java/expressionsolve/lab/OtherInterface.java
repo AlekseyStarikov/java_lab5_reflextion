a5118fdc3cd458e23387e164fb426f9d574a8606
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-23 05:31:40 +0300

    OtherInterface.java

--- src/main/java/expressionsolve/lab/OtherInterface.java
+++ src/main/java/expressionsolve/lab/OtherInterface.java
@@ -1,2 +1,10 @@
-package expressionsolve.lab;public interface OtherInterface {
+package expressionsolve.lab;
+/**
+ * @author Алексей Стариков
+ */
+public interface OtherInterface {
+    /**
+     * Выполняет действие, связанное с реализующим классом
+     */
+    void doSomething();
 }
