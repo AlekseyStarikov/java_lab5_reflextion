3fed9c56f49280058471526efc8ea31faf4c92b2
AlekseyStarikov <148627614+AlekseyStarikov@users.noreply.github.com>
2023-12-23 05:31:05 +0300

    OtherImpl.java

--- src/main/java/expressionsolve/lab/OtherImpl.java
+++ src/main/java/expressionsolve/lab/OtherImpl.java
@@ -1,2 +1,17 @@
-package expressionsolve.lab;public class OtherImpl {
+package expressionsolve.lab;
+
+/**
+ * @author Алексей Стариков
+ */
+public class OtherImpl implements SomeInterface {
+        /**
+         * Конструктор для класса OtherImpl
+         */
+        public OtherImpl() {}
+        /**
+         * Выполняет действие, связанное с этой реализацией, выводя в консоль "B"
+         */
+        public void doSomething() {
+            System.out.println("B");
+        }
 }
